cdbfa651f256060b377c9e3053a2426e58e418eb
Артем Мартынов <a.martynof2013@yandex.ru>
2022-05-07 10:16:51 +0300

    JavaHW22

--- src/BodyIndexService.java
+++ src/BodyIndexService.java
@@ -2,10 +2,10 @@ public class BodyIndexService {
 
     public double calculate(double growth, double weight) {
 
-        double InMetres = growth / 100;
-        double Squaring = InMetres * InMetres;
-        double BodyIndex = weight / Squaring;
+        double inMetres = growth / 100;
+        double squaring = inMetres * inMetres;
+        double bodyIndex = weight / squaring;
 
-        return BodyIndex;
+        return bodyIndex;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,10 @@ public class Main {
         double growth = 185.0;
         double weight = 90.0;
 
-        double Index = service.calculate (growth, weight);
-        System.out.println(Index);
+        double index = service.calculate (growth, weight);
+
+        System.out.println("Мой рост: " + growth + " см.");
+        System.out.println("Мой вес: " + weight + " кг.");
+        System.out.println("Индекс массы тела: " + index);
     }
 }
